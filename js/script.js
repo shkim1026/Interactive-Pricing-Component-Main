197790e9634b1ec1f2c378cb25ae16ea1b31ac8b
shkim1026 <60453195+shkim1026@users.noreply.github.com>
2021-05-18 02:11:43 -0700

    Adds CSS and JS

---
+++ js/script.js
@@ -0,0 +1,68 @@
+
+// VARIABLES START
+const pageviews = {
+    0: '10K Pageviews',
+    1: '50K Pageviews',
+    2: '100K Pageviews',
+    3: '500K Pageviews',
+    4: '1M Pageviews'
+};
+let cost = {
+    0: 8,
+    1: 12,
+    2: 16, 
+    3: 24,
+    4: 36
+};
+let discountedCost = {
+};
+const rangeFill = {
+    0: 'hsl(224, 65%, 95%)',
+    1: 'linear-gradient(to right, hsl(174, 77%, 80%) 25%, hsl(224, 65%, 95%) 25%)',
+    2: 'linear-gradient(to right, hsl(174, 77%, 80%) 50%, hsl(224, 65%, 95%) 50%)',
+    3: 'linear-gradient(to right, hsl(174, 77%, 80%) 75%, hsl(224, 65%, 95%) 75%)',
+    4: 'hsl(174, 77%, 80%)'
+};
+// VARIABLES END
+
+function checkPrice() {
+    let rangeValue = $('#tier-range').val()
+    for (i=0; i<=4; i++) {
+        if ($('#toggle').is(':checked')) {
+            let price = Object.values(cost)[i];
+            let discount = 0.25;
+            let calculatePrice = price - (price * discount);
+            let finalValue = calculatePrice.toFixed(2);
+            discountedCost[i] = finalValue;
+            if (rangeValue == i) {
+                let discountedPrice = Object.values(discountedCost)[i];
+                $('.payment').text('$' + discountedPrice);
+                let rangeFilled = Object.values(rangeFill)[i];
+                $('.range').css('background', rangeFilled);
+            }
+        } else if (rangeValue == i) {
+            let price = Object.values(cost)[i];
+            let priceDecimal = price.toFixed(2);
+            $('.payment').text('$' + priceDecimal);
+            let rangeFilled = Object.values(rangeFill)[i];
+            $('.range').css('background', rangeFilled);
+        }
+    }
+}
+
+$('#tier-range').on('input', function() {
+    checkPrice();
+});
+
+$('#toggle').on('change', function() {
+    checkPrice();
+});
+
+$(window).on("load resize", function() {
+    checkPrice();
+    if ($(window).width() > 767) {
+        $('.discount-text').text('25% discount');
+    } else {
+        $('.discount-text').text('-25%');
+    }
+});
